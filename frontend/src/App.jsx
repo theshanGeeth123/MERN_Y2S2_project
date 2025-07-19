4494f3fef722aed0361f7648366bb379f201719b
Theshan <theshangeethanjana@gmail.com>
2025-07-19 15:49:26 +0530

    Folder structure updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import CartPage from './shoppingCart/CartPage';
-import AddItemPage from './shoppingCart/AddItemPage';
-import EditItemPage from './shoppingCart/EditItemPage';
-import Nav from './shoppingCart/Nav';
-import Home from './shoppingCart/Home';
-import AllItemsPage from './shoppingCart/admin/AllItemsPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
@@ -14,12 +7,7 @@ function App() {
     <Router>
       <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/cart/new" element={<AddItemPage />} />
-        <Route path="/cart/edit/:id" element={<EditItemPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/adminCart" element={<AllItemsPage />} />
+        
       </Routes>
     </Router>
     
